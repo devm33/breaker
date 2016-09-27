48619af6454f110b86c5c13284ecc2f777b2b36b
Devraj Mehta <devm33@gmail.com>
2016-09-27 02:00:29 -0400

    shit this is a son of bitch

--- breaker/ViewController.swift
+++ breaker/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    let intervals = (1...12)
+        .map{$0 * 5}
+        .map{String($0)}
+
+    @IBOutlet weak var intervalPicker: UIPickerView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.intervalPicker.dataSource = self
+        self.intervalPicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // UIPicker impl
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return intervals.count;
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return intervals[row]
+    }
+
+    // Actions
+
+    @IBAction func startButton(_ sender: UIButton) {
+        sender.setTitle("Stop Working", for: UIControlState.normal)
+    }
 }
 
